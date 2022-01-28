e81566fc2b10ae01eebbd004583e953ef2e6a400
lenovo <lenovo@DESKTOP-4ALDDK7>
2022-01-28 12:25:47 +0530

    commit for jenkins execution

---
+++ src/test/java/com/crm/SDET292929/CreateContactTest.java
@@ -0,0 +1,16 @@
+package com.crm.SDET292929;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	@Test
+	public void createcontactTest(){
+		System.out.println("its somke test");
+	}
+	
+	@Test
+	public void organdcontacat(){
+		System.out.println("its one more smoke");
+	}
+
+}
---
+++ src/test/java/com/crm/SDET292929/CreateOrgTest.java
@@ -0,0 +1,11 @@
+package com.crm.SDET292929;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	@Test
+	public void createorgTest(){
+		System.out.println("its regression test");
+	}
+
+}
